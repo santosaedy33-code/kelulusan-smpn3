d16969da4f398f528de898e536b97bd4f85ab3ec
santosaedy33-code <santosaedy33@gmail.com>
2026-05-24 20:29:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const API_URL =
+"https://script.google.com/macros/s/AKFYCBXXXXXXX/exec";
+
+async function cekKelulusan(){
+
+const nis =
+document.getElementById("nis").value;
+
+const hasil =
+document.getElementById("hasil");
+
+hasil.innerHTML =
+"<p>Memproses...</p>";
+
+const response =
+await fetch(`${API_URL}?nis=${nis}`);
+
+const data =
+await response.json();
+
+if(!data.found){
+
+hasil.innerHTML =
+"<h3>Data tidak ditemukan</h3>";
+
+return;
+}
+
+hasil.innerHTML = `
+
+<img
+src="${data.foto}"
+class="foto">
+
+<h2>${data.nama}</h2>
+
+<p><b>NIS:</b> ${data.nis}</p>
+
+<p><b>Kelas:</b> ${data.kelas}</p>
+
+<p class="status">
+${data.status}
+</p>
+
+<p>${data.keterangan}</p>
+
+`;
+
+}
